d2877e025cd45874f83544e848f1f66af5234e22
TanguyHardelin <tanguy.hardelin@gmail.com>
2024-04-30 23:35:07 +0200

    Update CI

--- .ci/build_repository.sh
+++ .ci/build_repository.sh
@@ -8,6 +8,12 @@ sudo apt install libssl-dev
 sudo apt install libopencv-dev   
 sudo apt install libwebsocketpp-dev
 
+# Export environnement variables:
+export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/opt/local/lib/pkgconfig
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/local/lib
+
+# Uodate ldconfig
+sudo ldconfig
 
 # Create build directory
 mkdir build
