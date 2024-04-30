1b85d9d7ca7d061fc85a5f3e3f520f0bd775e435
TanguyHardelin <tanguy.hardelin@gmail.com>
2024-04-30 23:52:41 +0200

    Update CI

--- .ci/build_repository.sh
+++ .ci/build_repository.sh
@@ -2,16 +2,14 @@
 
 # Install dependency
 sudo apt update
-sudo apt upgrade
-sudo apt install build-essential
-sudo apt install cmake
-sudo apt install libssl-dev      
+# sudo apt upgrade
+sudo apt install build-essential cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev 
 sudo apt install libopencv-dev   
 sudo apt install libwebsocketpp-dev
 
 # Export environnement variables:
-export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/opt/local/lib/pkgconfig
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/local/lib
+export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/usr/local/lib/pkgconfig
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
 
 # Uodate ldconfig
 sudo ldconfig
