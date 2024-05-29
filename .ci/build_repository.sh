26e1ad8cd25c4baf4af4a6f31167b09fc7053c89
TanguyHardelin <tanguy.hardelin@gmail.com>
2024-05-29 20:50:56 +0200

    Update actions and scripts

--- .ci/build_repository.sh
+++ .ci/build_repository.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Install dependency
-sudo apt update
-# sudo apt upgrade
-sudo apt install build-essential cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev 
-sudo apt install libopencv-dev   
-sudo apt install libwebsocketpp-dev
 
 # Uodate ldconfig
 sudo ldconfig
